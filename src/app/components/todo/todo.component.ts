61da95b0a5d8892d4b52db62a0c4abc9a9008459
Mary <mary_hbb@yahoo.com>
2023-02-26 00:36:14 +0100

    add AngularFirestoreCollection in todo service

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TodoComponent {
 
+  onClick(titleInput: HTMLInputElement){
+  titleInput.value
+  }
 }
--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  firestoreCollection: AngularFirestoreCollection;
+
+  constructor(private firestore: AngularFirestore) {
+
+    this.firestoreCollection = firestore.collection('todos')
+  }
 }
